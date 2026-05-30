8fcb18728c6f5e6df759447bdba4ed56ffc306ce
Abel Ruiz <abelruiz@MacBook-Air-de-Abel.local>
2026-05-29 19:16:39 -0600

    Desactivar modo mantenimiento

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,7 +1,7 @@
 import { type NextRequest, NextResponse } from 'next/server'
 import { updateSession } from '@/lib/supabase/middleware'
 
-const MAINTENANCE = true
+const MAINTENANCE = false
 
 export async function proxy(request: NextRequest) {
   if (MAINTENANCE) {
